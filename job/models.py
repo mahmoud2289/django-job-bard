e97aebc668000bf616086b81f9bf8ee5fc55729b
Mahmoud Khalaf <m.khalaf2289@nu.edu.eg>
2024-11-29 22:05:20 +0200

    commititng modifications in job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+#job
+#job again
+
+'''
+ django model field : 
+    - html widget
+    - validation 
+    - db size 
+'''
+
+class Job(models.Model): 
+    title = models.CharField(max_length=100)  # column
+def __str__(self):
+    return self.title
+    
